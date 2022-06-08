8407d2cedee45028ec51c207bf88f343176fc78a
Omar-Gad <omargad9000@gmail.com>
2022-06-08 11:20:21 +0200

    Added quantity and total functionality to cart

--- account-service/cart/views.py
+++ account-service/cart/views.py
@@ -28,6 +28,35 @@ class CartItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         cart = CartDetailView.get_object(self)
         return CartItem.objects.filter(cart=cart.id)
+    
+    
+    def perform_update(self, serializer):
+        instance = self.get_object()
+        cart = self.request.user.cart
+        
+        old_quantity = instance.quantity
+        
+        serializer.save()
+        
+        cart.quantity -= old_quantity
+        cart.total -= old_quantity * serializer.data['product']['price']
+        
+        cart.quantity += serializer.data['quantity']
+        cart.total += serializer.data['quantity']*serializer.data['product']['price']
+        
+        cart.save()
+        
+
+    def perform_destroy(self, instance):
+        cart = self.request.user.cart
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+
+        cart.total -= data['product']['price'] * data['quantity']
+        cart.quantity -= instance.quantity
+        cart.save()
+
+        instance.delete()
 
 
 class CartItemCreateView(generics.CreateAPIView):
@@ -41,21 +70,29 @@ class CartItemCreateView(generics.CreateAPIView):
         cart = request.user.cart
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data.update({'cart':cart})
+
+        serializer.validated_data.update({'cart': cart})
         self.perform_create(serializer)
+
+        cart.quantity += serializer.data['quantity']
+        cart.total += serializer.data['product']['price'] * serializer.data['quantity']
+        cart.save()
+
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
+
 
 class CartCheckout(views.APIView):
-    authentication_classes=(TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
+
     def get(self, request, format=None, *args, **kwargs):
         cart = request.user.cart
         cart_items = CartItem.objects.filter(cart=cart)
         order = Order.objects.create(total=cart.total, user=cart.user)
         for item in cart_items:
-            OrderItem.objects.create(quantity=item.quantity, product=item.product, order=order)
+            OrderItem.objects.create(
+                quantity=item.quantity, product=item.product, order=order)
         cart.delete()
         Cart.objects.create(user=request.user)
         new_order = OrderSerializer(order).data
-        return Response({'New order':new_order})
+        return Response({'New order': new_order})
